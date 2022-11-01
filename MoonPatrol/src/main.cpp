4c7bb0ae60bad9e882ab75032866a632affb0a60
JMG3N0 <joan.rivas.cepeda@gmail.com>
2022-11-01 10:46:23 -0300

    Collision Added

--- MoonPatrol/src/main.cpp
+++ MoonPatrol/src/main.cpp
@@ -7,23 +7,42 @@ int main()
 
     InitWindow(screenWidth, screenHeight, "MoonPatrol");
 
-    Vector2 CubePlayer = { 360, 480 };
-    Vector2 ObstacleRectangle = { 900, 470 };
+    float InitialPosPla[2] = {360, 480};
+    float InitialPosObs[2] = {900, 470};
+
+    Rectangle CubePlayer = { InitialPosPla[0], InitialPosPla[1], 20, 20 };
+    Rectangle ObstacleRectangle = { InitialPosObs[0], InitialPosObs[1], 20, 60 };
     bool KeyPressed = false;
     int timer = 0;
     bool Negtimer = false;
     int timerObs =0;
+    int seconds = 0;
+    int minutes = 0;
+    int counter = 0;
 
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        
+        if (counter < 60)
+        {
+            counter++;
+        }
+        if (counter == 60)
+        {
+            seconds++;
+            counter = 0;
+        }
+        if (seconds == 60)
+        {
+            minutes++;
+            seconds = 0;
+        }
 
 
         if (KeyPressed == false)
         {
-         if (IsKeyPressed(KEY_A))
+         if (IsKeyPressed(KEY_W))
             {
              KeyPressed = true;
              timer = 10;
@@ -69,21 +88,29 @@ int main()
            
         }
 
-     //   timerObs++;
-
-       // if (timerObs == 60)
-      //  {
-                if (ObstacleRectangle.x > 0)
+               if (ObstacleRectangle.x > 0)
                 {
                     ObstacleRectangle.x-=12;
-                 }
+                }
                 if (ObstacleRectangle.x <= 0)
-                    {
+                {
                     ObstacleRectangle.x = 900;
-                    }
-           //     timerObs = 0;
-      //  }
-        
+                }
+       
+
+                if (CheckCollisionRecs(CubePlayer, ObstacleRectangle) == true)
+                {
+                    CubePlayer.x = InitialPosPla[0];
+                    CubePlayer.y = InitialPosPla[1];
+                    ObstacleRectangle.x = InitialPosObs[0];
+                    ObstacleRectangle.y = InitialPosObs[1];
+                    seconds = 0;
+                    minutes = 0;
+                    counter = 0;
+                }
+
+                
+               
 
         BeginDrawing();
 
@@ -92,7 +119,9 @@ int main()
         DrawRectangle(ObstacleRectangle.x, ObstacleRectangle.y, 20, 60, GREEN);
 
         DrawText("Version 0.1", 900, 700, 20, RED);
-    
+        DrawText(TextFormat("%02i", minutes), 450,200, 20, GREEN);
+        DrawText(TextFormat(":%02i", seconds), 473,200, 20, GREEN);
+        
     
         EndDrawing();
     }
